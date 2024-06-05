5deeba32ded736527277e6f38f4768326e9232ef
Abel <abelsantiagocortes@gmail.com>
2024-06-05 17:30:51 -0500

    BDD3806[config] Add missing dependencies to run Vue component

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
 const path = require('path');
+const packageJson =  require('./package.json');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  mode: 'development',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './dist/index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
+module.exports = ({ mode }) => {
+  return {
+    mode: mode,
+    entry: './src/index.js',
+    output: {
+      filename: `conversa-${packageJson.version}.js`,
+      path: path.resolve(__dirname, 'dist'),
+      library: {
+        type: 'umd'
+      }
+    },
+    devServer: {
+      static: './dist',
+      port: 8083,
+      client: { overlay: true },
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './dist/index.html',
+      }),
+      new VueLoaderPlugin()
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
           loader: 'babel-loader',
         },
-      },
-    ],
-  },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.pug$/,
+          use: 'pug-plain-loader'
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'postcss-loader'],
+        }
+      ],
+    },
+  };
 };
